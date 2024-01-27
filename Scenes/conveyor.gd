b344530bc1bac4e04f0d6d6799811be264e89930
CISAC <carolisacosta99@gmail.com>
2024-01-27 01:24:45 +0000

    Working conveyor belts

--- Scenes/conveyor.gd
+++ Scenes/conveyor.gd
@@ -1,12 +1,12 @@
 extends Node3D
 
 var speed = 2
-
-@export var rigidbody : RigidBody3D
+var direction = Vector3.ZERO
+var target : Marker3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	target = $Target
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -16,13 +16,27 @@ func _physics_process(_delta):
 	pass
 
 func _on_area_3d_body_entered(body):
-	#print("body entered")
-	body.moving = true
-	body.movement_speed = speed
-	body.direction = transform.basis.z
+	#body.moving = true
+	if !body.current_conveyor:
+		body.current_conveyor = self
+		body.moving = true
+		body.movement_speed = speed
+		#print("this should only run once")
+	#body.target_point = target
+	#body.direction = transform.basis.z
+	print(body.name + ": " + str(body.current_conveyor))
+	
+	#if body.is_in_group("Movable"):
+		#body.direction = ($Target.position - body.position).normalized()
+	#
+	#print(name + " body entered: " + body.name)
+	#body.moving = true
+	#body.movement_speed = speed
+	#body.direction = transform.basis.z
 	#body.move(transform.basis.z)
 
 
 func _on_area_3d_body_exited(body):
-	body.moving = false
+	print(name + " body exited: " + body.name)
+	#body.moving = false
 	pass # Replace with function body.
--- Scenes/movable_object.gd
+++ Scenes/movable_object.gd
@@ -3,6 +3,9 @@ extends RigidBody3D
 var moving = false
 var direction
 var movement_speed = 2
+var current_conveyor
+var target_point
+var target_marker
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +13,69 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	print(str(moving) + ', ' + str(direction) + ', ' + str(movement_speed))
+	if current_conveyor:
+		if is_same_direction(direction, Vector3.ZERO):
+				direction = current_conveyor.direction
+	
+	if target_marker:
+		var target_marker_owner = target_marker.get_owner()
+		#print(
+			#"moving: " + str(moving) +
+			#', direction: ' + str(direction) +
+			#', movement_speed: ' + str(movement_speed) +
+			#', current_conveyor: ' + str(current_conveyor) +
+			#', target_point: ' + str(target_point) +
+			#', target_marker: ' + str(target_marker_owner) +
+			#'\n-------------------------------------------------'
+		#)
 	pass
 	
 func _physics_process(delta):
-	if moving:
-		move_and_collide(direction * movement_speed * delta)
+	if current_conveyor && moving:
+		move_towards_target(delta)
+		
+func move_towards_target(delta):
+	target_point = current_conveyor.target.global_position
+	target_marker = current_conveyor.target
+
+	direction = (target_point - global_position).normalized()
+	direction.y = 0
+
+	var motion = direction * movement_speed * delta
+	move_and_collide(motion)
+
+	if global_position.distance_squared_to(target_point) < 0.1:
+		
+		for area in $Area3D.get_overlapping_areas():
+			if area.is_in_group("Conveyor Area"):
+				current_conveyor = area.get_owner()
+		
+		print("change direction?")
+		print("current conveyor: " + str(current_conveyor))
+
+		if !is_same_direction(current_conveyor.direction, direction):
+			print("change direction")
+			change_direction()
+		else:
+			print("same direction")
+			moving = true
+			
+func is_same_direction(a: Vector3, b: Vector3) -> bool:
+	# Compare vectors using a threshold (epsilon)
+	var dot_product = a.normalized().dot(b.normalized())
+	var magnitude_product = a.length() * b.length()
+	return abs(dot_product - magnitude_product) < 0.001
+
+func change_direction():
+	
+	for area in $Area3D.get_overlapping_areas():
+		if area.is_in_group("Conveyor Area"):
+			current_conveyor = area.get_owner()
+			target_point = current_conveyor.target.global_position
+			target_marker = current_conveyor.target
+			break
+			
+	moving = true
+
+	var target_marker_owner = target_marker.get_owner()
+	print("Changed direction to: " + str(current_conveyor) + ", target_marker_owner: " + str(target_marker_owner))
